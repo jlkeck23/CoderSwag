341798a8e21a3ee2c3e612fe01d472aa510f725a
Jennifer <jleannk23@gmail.com>
2018-04-27 11:11:58 -0600

    added in all the discp

--- app/src/main/java/com/jenk/coderswag/Services/DataService.kt
+++ app/src/main/java/com/jenk/coderswag/Services/DataService.kt
@@ -19,4 +19,19 @@ object DataService {
             Product("Devslopes Hat Snapback","$22","hat04")
     )
 
+    val hoodies = listOf(
+            Product("Devslopes Hoodie Grey", "$28","hoodie01"),
+            Product("Devslopes Hoodie Red", "$32","hoodie02"),
+            Product("Devslopes Grey Hoodie", "$28","hoodie03"),
+            Product("Devslopes Black Hoodie", "$32","hoodie04")
+    )
+
+    val shirts = listOf(
+            Product("Devslopes Shirt Black", "$18","shirt01"),
+            Product("Devslopes Badge Light Grey", "$20","shirt02"),
+            Product("Devslopes Logo Shirt Red", "$22","shirt03"),
+            Product("Devslopes Hustle", "$22","shirt04"),
+            Product("Kickflip Studio","18","shirt05")
+    )
+
 }
