2e2b339ec9651eecb031ba231ec988a77cc1c9b9
Jennifer <jleannk23@gmail.com>
2018-04-27 14:39:24 -0600

    prepare

--- app/src/main/java/com/jenk/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/jenk/coderswag/Controller/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jenk.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import com.jenk.coderswag.Adapters.CategoryAdapter
 import com.jenk.coderswag.R
 import com.jenk.coderswag.Services.DataService
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter=adapter
+
 
     }
 }
