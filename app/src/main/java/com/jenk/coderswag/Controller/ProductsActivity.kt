3a6c6aebf13210597d5bf3d1c6ec05d019e5e060
Jennifer <jleannk23@gmail.com>
2018-04-30 08:33:18 -0600

    added list views screen lay out custom adapter

--- app/src/main/java/com/jenk/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/jenk/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,43 @@
 package com.jenk.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.widget.GridLayout
+import com.jenk.coderswag.Adapters.ProductsAdapter
 import com.jenk.coderswag.R
+import com.jenk.coderswag.Services.DataService
 import com.jenk.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+
+        var spanCount =2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        //extra large screen sizes start at 720dp
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/jenk/coderswag/Services/DataService.kt
+++ app/src/main/java/com/jenk/coderswag/Services/DataService.kt
@@ -16,10 +16,27 @@ object DataService {
             Product("Devslopes Graphic Beanie","$18","hat1"),
             Product("Devslopes Hat Black","$20","hat2"),
             Product("Devslopes Hat White","$18","hat3"),
+            Product("Devslopes Hat Snapback","$22","hat4"),
+            Product("Devslopes Graphic Beanie","$18","hat1"),
+            Product("Devslopes Hat Black","$20","hat2"),
+            Product("Devslopes Hat White","$18","hat3"),
+            Product("Devslopes Hat Snapback","$22","hat4"),
+            Product("Devslopes Graphic Beanie","$18","hat1"),
+            Product("Devslopes Hat Black","$20","hat2"),
+            Product("Devslopes Hat White","$18","hat3"),
             Product("Devslopes Hat Snapback","$22","hat4")
+
     )
 
     val hoodies = listOf(
+            Product("Devslopes Hoodie Grey", "$28","hoodie1"),
+            Product("Devslopes Hoodie Red", "$32","hoodie2"),
+            Product("Devslopes Grey Hoodie", "$28","hoodie3"),
+            Product("Devslopes Black Hoodie", "$32","hoodie4"),
+            Product("Devslopes Hoodie Grey", "$28","hoodie1"),
+            Product("Devslopes Hoodie Red", "$32","hoodie2"),
+            Product("Devslopes Grey Hoodie", "$28","hoodie3"),
+            Product("Devslopes Black Hoodie", "$32","hoodie4"),
             Product("Devslopes Hoodie Grey", "$28","hoodie1"),
             Product("Devslopes Hoodie Red", "$32","hoodie2"),
             Product("Devslopes Grey Hoodie", "$28","hoodie3"),
@@ -31,7 +48,17 @@ object DataService {
             Product("Devslopes Badge Light Grey", "$20","shirt2"),
             Product("Devslopes Logo Shirt Red", "$22","shirt3"),
             Product("Devslopes Hustle", "$22","shirt4"),
-            Product("Kickflip Studio","18","shirt5")
+            Product("Kickflip Studio","$18","shirt5"),
+            Product("Devslopes Shirt Black", "$18","shirt1"),
+            Product("Devslopes Badge Light Grey", "$20","shirt2"),
+            Product("Devslopes Logo Shirt Red", "$22","shirt3"),
+            Product("Devslopes Hustle", "$22","shirt4"),
+            Product("Kickflip Studio","$18","shirt5"),
+            Product("Devslopes Shirt Black", "$18","shirt1"),
+            Product("Devslopes Badge Light Grey", "$20","shirt2"),
+            Product("Devslopes Logo Shirt Red", "$22","shirt3"),
+            Product("Devslopes Hustle", "$22","shirt4"),
+            Product("Kickflip Studio","$18","shirt5")
     )
 
     val digitalGoods = listOf<Product>()
